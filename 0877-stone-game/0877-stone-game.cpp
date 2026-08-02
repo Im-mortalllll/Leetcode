a32413c6389e4c0a6c0e60d8655f539dfbe61abd
Amar Gupta <amar28032007@gmail.com>
2026-08-02 07:33:56 +0530

    Time: 0 ms (100%), Space: 10.2 MB (98.71%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,23 +1,6 @@
 class Solution {
-private:
-    int recur(vector<int> &v, int l, int r, vector<vector<int>> &re){
-        if (l==r){
-            return v[l];
-        }
-        if(re[l][r]!=INT_MIN){
-            return re[l][r];
-        }
-        int left=v[l]-recur(v,1+l,r,re);
-        int right= v[r]-recur(v,l,r-1,re);
-        return re[l][r]=max(left,right);
-    }
 public:
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        vector<vector<int>> re(n, vector<int> (n,INT_MIN));
-        if(recur(piles,0,n-1,re)>0){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
