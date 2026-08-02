874d92318bf75440d3cfe7eee14fb9ec03f32d6b
Amar Gupta <amar28032007@gmail.com>
2026-08-02 08:24:39 +0530

    Time: 0 ms (100%), Space: 20.9 MB (20.38%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int>ans(nums.size(),0);
-        for(int i=0;i<nums.size();i++){
-            ans[i]=nums[nums[i]];
+        vector<int>arr;
+        for(int i = 0; i < nums.size(); i++){
+            arr.push_back(nums[nums[i]]);
         }
-        return ans;
+        return arr;
     }
 };
