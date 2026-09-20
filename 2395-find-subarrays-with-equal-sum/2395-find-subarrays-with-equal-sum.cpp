fd882be9ef84ae88151d6fc46bd65073a3749009
Amar Gupta <amar28032007@gmail.com>
2026-09-20 14:39:49 +0530

    Time: 0 ms (100%), Space: 11.5 MB (24.56%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool findSubarrays(vector<int>& nums) {
+        unordered_map<int, int> mp(nums.size());
+        for (int i = 0 ; i < nums.size() - 1; i++){
+            int sum = nums[i]+nums[i+1];
+            if (mp.count(sum)){
+                return true;
+            }
+            mp[sum] = i;
+        }
+        return false;
+    }
+};
