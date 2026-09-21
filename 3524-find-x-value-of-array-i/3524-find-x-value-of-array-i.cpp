695aad0bd5d33e3b90d9998e620b024b9c7bb4cb
Amar Gupta <amar28032007@gmail.com>
2026-09-21 22:34:18 +0530

    Time: 165 ms (44.64%), Space: 151.2 MB (55.36%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    // int find_prod(int a, int b, vector <int> &nums, int &k){
+    //     int prod = 1;
+    //     for (int i = a; i <= b; i++){
+    //         prod = (prod * nums[i]) % k;
+    //     }
+    //     return prod;
+    // }
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long int> result(k,0);
+        vector<long long int> prev_cnt(k,0);
+        for (int i = 0; i< nums.size(); i++){
+            vector<long long int> curr_cnt(k,0);
+            curr_cnt[nums[i]%k]++;
+            for (int old = 0; old < k; old++){
+                int neww = ((long long)old * nums[i] % k) % k;
+                curr_cnt[neww] += prev_cnt[old];
+            }
+            prev_cnt = move(curr_cnt);
+            for (int x =0; x <= k-1; x++){
+                result[x] += prev_cnt[x];
+            }
+        }
+        return result;
+    }
+};
